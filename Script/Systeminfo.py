7d077a78366d7e4529299a0e69398b14ca90fe72
X-Cyborg <puneethk.cs18@sahyadri.edu.in>
2020-10-01 12:00:24 +0530

    Create Systeminfo.py

---
+++ Script/Systeminfo.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import platform
+
+print("System:-")
+print(os.name)
+print(platform.system(),platform.release())
+print("----------")
+print("Python Version:-")
+print(sys.version)
